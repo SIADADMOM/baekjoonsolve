3293afc3fca4f119776c98d01f740d7b6d65b2ce
SIA <realk12345@gmail.com>
2023-05-20 00:20:05 +0900

    [Bronze V] Title: A+B - 7, Time: 116 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const num = Number(input[0]);
+let answer = '';
+
+for (let i = 1; i <= num; i++) {
+    let data = input[i].split(' ');
+    answer += `Case #${i}: ${Number(data[0]) + Number(data[1])}` + "\n";
+    
+}
+
+console.log(answer);
